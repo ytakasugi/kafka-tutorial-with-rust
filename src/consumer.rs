db904f32209040283c7b20c5a571d57cc4feab59
ytakasugi <sh7.tibi0129@gmail.com>
2024-01-02 00:34:03 +0900

    Change files

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,33 +1,52 @@
-use rdkafka::{ClientConfig, Message};
 use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::{ClientConfig, Message};
 
 use futures::TryStreamExt;
 
+struct KafkaConsumer<'a> {
+    consumer: StreamConsumer,
+    topic: &'a str,
+}
+
+impl<'a> KafkaConsumer<'a> {
+    fn new(topic: &'a str) -> Self {
+        let consumer: StreamConsumer = ClientConfig::new()
+            .set("bootstrap.servers", "localhost:29092")
+            .set("enable.partition.eof", "false")
+            .set("group.id", "rust-kafka-group-1")
+            .create::<StreamConsumer>()
+            .expect("Failed to create client");
+
+        Self {
+            consumer: consumer,
+            topic: topic,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let consumer = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:19092")
-        .set("enable.partition.eof", "false")
-        .set("group.id", "rust-kafka-group-1")
-        .create::<StreamConsumer>()
-        .expect("Failed to create client");
+    let consumer: KafkaConsumer = KafkaConsumer::new("RUST-KAFKA-TOPIC-1");
 
-    consumer.subscribe(&["RUST-KAFKA-TOPIC-1"]).unwrap();
+    consumer.consumer.subscribe(&[consumer.topic]).unwrap();
 
-    let stream_processor = consumer.stream().try_for_each(|msg| {
-        async move {
-            println!("Received message from topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-                     msg.topic(), msg.partition(), msg.offset(), msg.timestamp());
+    let stream_processor = consumer.consumer.stream().try_for_each(|msg| async move {
+        println!(
+            "Received message from topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+            msg.topic(),
+            msg.partition(),
+            msg.offset(),
+            msg.timestamp()
+        );
 
-            tokio::time::sleep(tokio::time::Duration::from_millis(3_000)).await;
-            let r = match msg.payload_view::<str>() {
-                Some(Ok(payload)) => format!("Payload len for {} is {}", payload, payload.len()),
-                Some(Err(_)) => "Message payload is not a string".to_owned(),
-                None => "No payload".to_owned(),
-            };
-            println!("{}", r);
-            Ok(())
-        }
+        tokio::time::sleep(tokio::time::Duration::from_millis(3_000)).await;
+        let r = match msg.payload_view::<str>() {
+            Some(Ok(payload)) => format!("Payload len for {} is {}", payload, payload.len()),
+            Some(Err(_)) => "Message payload is not a string".to_owned(),
+            None => "No payload".to_owned(),
+        };
+        println!("{}", r);
+        Ok(())
     });
 
     stream_processor.await.expect("stream processing failed");
--- src/producer.rs
+++ src/producer.rs
@@ -1,19 +1,37 @@
-use rdkafka::ClientConfig;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::util::Timeout;
+use rdkafka::ClientConfig;
+
+#[derive(Clone)]
+struct KafkaProducer<'a> {
+    producer: FutureProducer,
+    topic: &'a str,
+}
+
+impl<'a> KafkaProducer<'a> {
+    fn new(topic: &'a str) -> Self {
+        let producer: FutureProducer = ClientConfig::new()
+            .set("bootstrap.servers", "localhost:29092")
+            .set("message.timeout.ms", "5000")
+            .create()
+            .expect("Producer creation error");
+
+        Self {
+            producer: producer,
+            topic: topic,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:19092")
-        .set("message.timeout.ms", "5000")
-        .create()
-        .expect("Producer creation error");
+    let producer: KafkaProducer = KafkaProducer::new("RUST-KAFKA-TOPIC-1");
 
     producer
+        .producer
         .send(
-            FutureRecord::<(), _>::to("RUST-KAFKA-TOPIC-1").payload("Hello Rust and Kafka")
-            , Timeout::Never
+            FutureRecord::<(), _>::to(producer.topic).payload("Hello Rust and Kafka"),
+            Timeout::Never,
         )
         .await
         .expect("Failed to produce");
